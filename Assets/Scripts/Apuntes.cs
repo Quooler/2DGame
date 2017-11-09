34618881b9e17422b554e08d7316f7c72734c093
Quooler <marc.vallverdu.98@gmail.com>
2017-11-09 23:33:19 +0100

    Added Project

---
+++ Assets/Scripts/Apuntes.cs
@@ -0,0 +1,10 @@
+﻿/* Pixels per unit tienen que ser iguales en todos los sprites para que no haya problemas de escala. 
+ * 
+ * Pivote de los players abajo (bottom), pivote de las tiles arriba (top).
+ * 
+ * Sorting layer define el orden de dibujado de los sprites a nivel de profundidad-. 
+ * 
+ * Type full rect para las tiles.
+ * 
+ * */
+
