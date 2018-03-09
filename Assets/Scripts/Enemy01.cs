11d4a0628622d8f573d3fc5a4c12958ce58bb236
Quooler <marc.vallverdu.98@gmail.com>
2018-03-09 11:58:40 +0100

    Fixed some issues and added more thuings

--- Assets/Scripts/Enemy01.cs
+++ Assets/Scripts/Enemy01.cs
@@ -1,10 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement;
+using System; 
 
 public class Enemy01 : MonoBehaviour
 {
+    Transform[] pathPoints;
+    bool isIndexing;
+    int index; 
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, pathPoints[index].position, Time.deltaTime);
+
+        if(transform.position == pathPoints[index].position)
+        {
+            isIndexing = !isIndexing;
+            index = Convert.ToInt32(isIndexing);        
+        }
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
