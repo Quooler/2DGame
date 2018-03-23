e458031412c7a072c603ee77d6d7c714b1dc130a
Quooler <marc.vallverdu.98@gmail.com>
2018-03-23 18:30:59 +0100

    Added title scene & scrolling background

---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    [SerializeField]
+    float scrollSpeed;
+    [SerializeField]
+    float tileSizeX;
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeX);
+        transform.position = startPosition + Vector3.right * newPosition;
+    }
+}
